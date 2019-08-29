00167c120b3aeac4a987d8b029ec1e0582bccbe2
Greg Doud <gpdoud@gmail.com>
2019-08-29 13:55:28 -0400

    Add project files.

---
+++ FriendsProject/Friend.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FriendsProject {
+    class Friend {
+
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public bool BFF { get; set; } = false;
+        public int HowLong { get; set; }
+        
+
+        public Friend(string name, string phone, string email, bool bff, int howlong) {
+            this.Name = name;
+            this.Phone = phone;
+            this.Email = email;
+            this.BFF = bff;
+            this.HowLong = howlong;
+        }
+        public Friend() {
+
+        }
+        public override string ToString() {
+            return $"{Name} {Phone} {Email} {(BFF ? "BFF" : "")} {HowLong}";
+        }
+    }
+}
---
+++ FriendsProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FriendsProject {
+    class Program {
+        static void Main(string[] args) {
+
+            var f1 = new Friend("Jim", "513-555-1212", "fa@mail.com", true, 3);
+            var f2 = new Friend("Alice", "513-555-1212", "fb@mail.com", true, 2);
+            var f3 = new Friend("Bill", "513-555-1212", "fc@mail.com", false, 5);
+            var f4 = new Friend("Kate", "513-555-1212", "fd@mail.com", false, 1);
+            var f5 = new Friend("Pat", "513-555-1212", "fe@mail.com", false, 4);
+
+            var friends = new Friend[] { f1, f2, f3, f4, f5 };
+
+            foreach(var friend in friends) {
+                Console.WriteLine(friend);
+            }
+        }
+    }
+}
